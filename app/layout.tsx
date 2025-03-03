1ac9a966a6a8fe68e929d96d77f9bb56e5bb9da6
tomaslau <hi@tomaslau.com>
2025-03-03 15:15:00 +0100

    added x-robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,17 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
 };
 
 export default function RootLayout({
